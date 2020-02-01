d7c7e91e4b9b7ad3f8882e47117855f6b1241620
LakerG <kuzmicnemanja@live.com>
2020-02-01 10:27:01 +0100

    Kind of alpha version.

---
+++ Ocosave/Assets/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    [SerializeField] private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("IsActive");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            anim.ResetTrigger("IsActive");
+        }
+	}
+}
---
+++ Ocosave/Assets/EntityKiller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityKiller : MonoBehaviour
+{
+    private bool hasKill = false;
+    private float elapsedTime = 0.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasKill)
+        {
+            Destroy(collision.gameObject);
+            hasKill = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (hasKill)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime > 0.5f)
+            {
+                hasKill = false;
+                elapsedTime = 0.0f;
+            }
+        }
+    }
+}
